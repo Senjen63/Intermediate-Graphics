5800637b01407ca3435a0ab248ffee030a017ac2
Eugene <eugene.bryant@mymail.champlain.edu>
2023-02-08 17:55:04 -0500

    Finished Assignment 2

--- GPR300_Transformations_Template/GPR300_Transformations_Template/GPR300_Transformations_Template/GPR300_Transformations/main.cpp
+++ GPR300_Transformations_Template/GPR300_Transformations_Template/GPR300_Transformations_Template/GPR300_Transformations/main.cpp
@@ -184,16 +184,12 @@ struct Camera
 	float deltaTime = glfwGetTime();
 	float speed = 0.5f;
 	float radius = 5.0f;
-	//glm::vec3 position = glm::vec3(0, 0, 5);
 	glm::vec3 position = glm::vec3(1);
 	
 	glm::vec3 target = glm::vec3(0, 0, 0); //world position to look at
 	float fov = 50.0f; //vertical field of view
 	float orthographicSize = 30.0f; //height of frustum in view space
 	bool orthographic;
-	
-	//float currentTime;
-	//float previousTime = currentTime;
 	
 
 	glm::mat4 getViewMatrix()
@@ -243,7 +239,6 @@ struct Camera
 		o[3][2] = -(farPlane + nearPlane) / (farPlane - nearPlane);
 		o[3][3] = 1;
 
-		//return glm::mat4(1);
 		return o;
 	}
 
@@ -259,9 +254,7 @@ struct Camera
 		p[2][3] = -1;
 		p[3][2] = -2 * (farPlane * nearPlane) / (farPlane - nearPlane);
 		p[3][3] = 1;
-		
 
-		//return glm::mat4(1);
 		return p;
 	}
 
@@ -390,7 +383,7 @@ int main() {
 		//Draw UI
 		ImGui::Begin("Settings");
 		ImGui::SliderFloat("Orbit radius", &camera.radius, 0.0f, 100.0f);
-		ImGui::SliderFloat("Orbit speed", &camera.speed, 0.0f, 100.0f);
+		ImGui::SliderFloat("Orbit speed", &camera.speed, 0.0f, 10.0f);
 		ImGui::SliderFloat("Field of View", &camera.fov, 0.0f, 100.0f);
 		ImGui::SliderFloat("Orthographic height", &camera.orthographicSize, 0.0f, 100.0f);
 		ImGui::Checkbox("Orthographic toggle", &camera.orthographic);
